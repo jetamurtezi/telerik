af79d9c575b19f486a967d456799f1027c3cb9c1
jetamurtezi <jetamurtezi98@gmail.com>
2025-06-27 08:44:12 +0200

    initial commit

---
+++ Controllers/GridController.cs
@@ -0,0 +1,32 @@
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using telerik.Models;
+
+namespace telerik.Controllers
+{
+    public class GridController : Controller
+    {
+
+        public IActionResult Customers()
+        {
+            return View();
+        }
+        public ActionResult Customers_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            var result = Enumerable.Range(0, 50).Select(i => new Customer
+            {
+                CompanyName = "Company Name " + i,
+                ContactName = "Contact Name " + i,
+                ContactTitle = "Contact Title " + i,
+                Country = "Coutry " + i
+            });
+
+            var dsResult = result.ToDataSourceResult(request);
+            return Json(dsResult);
+        }
+
+        
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using telerik.Models;
+
+namespace telerik.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
